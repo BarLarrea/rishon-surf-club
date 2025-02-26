8743d90e0fc2c0590381c886728aff195bfc3da0
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2025-02-26 11:21:46 +0200

    Create the login and register kotlin ftagments and use it in main activity

--- app/src/main/java/com/example/surf_club_android/MainActivity.kt
+++ app/src/main/java/com/example/surf_club_android/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.surf_club_android
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.surf_club_android.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            showLoginFragment()
         }
+    }
+
+    private fun showLoginFragment() {
+        replaceFragment(LoginFragment())
+    }
+
+    private fun showRegisterFragment() {
+        replaceFragment(RegisterFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun navigateToRegister() {
+        showRegisterFragment()
+    }
+
+    fun navigateToLogin() {
+        showLoginFragment()
     }
 }
